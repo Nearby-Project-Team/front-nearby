aeec69af32255dbb644933716368081eed82943c
Subin Park <lmhapy25@khu.ac.kr>
2022-07-14 16:15:38 +0900

    Set message list

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:front_nearby/palette.dart';
+import 'package:logger/logger.dart';
 
 class HomePage extends StatefulWidget{
   @override
@@ -16,35 +17,40 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Chat App"),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 8.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _textEditingController,
-                decoration: InputDecoration( hintText: "메시지 입력창"),
-                onSubmitted: _handleSubmitted,
-              ),
+      body: Column(
+        children: [
+          Expanded(child:ListView(),),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration( hintText: "메시지 입력창"),
+                      onSubmitted: _handleSubmitted,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 0.8,
+                  ),
+                  TextButton(
+                      onPressed: (){
+                        _handleSubmitted(_textEditingController.text);
+                      },
+                      style: TextButton.styleFrom(
+                          primary: Palette.newBlue),
+                      child: const Text("전송"))
+                ]
             ),
-            const SizedBox(
-              width: 0.8,
-            ),
-            TextButton(
-                onPressed: (){
-                  _handleSubmitted(_textEditingController.text);
-                },
-                style: TextButton.styleFrom(
-                  primary: Palette.newBlue),
-                child: const Text("전송"))
-          ]
-        ),
-      ),
+          ),
+        ],
+      )
     );
   }
 
   void _handleSubmitted(String text){
-    print(text);
+    Logger().d(text);
     _textEditingController.clear();
   }
 }
